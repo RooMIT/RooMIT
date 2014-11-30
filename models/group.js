af3d02943a3f01a75e7e57307a013d44e4fe099b
Peinan Chen <peinanchen@gmail.com>
2014-11-30 12:43:27 -0500

    reverted group

--- models/group.js
+++ models/group.js
@@ -6,9 +6,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var ObjectId = Schema.Types.ObjectId;
 
-var GroupSchema = new Schema({
-	users: [{ type: ObjectId, ref: 'User' }]
-});
+var GroupSchema = new Schema();
 
 var Group = mongoose.model('Group', GroupSchema);
 module.exports = Group;
